9034ccb34f509c9baeb88de5a95f51ea584a3ed0
Guilherme Custodio Nieto <guilhermecustodionieto@gmail.com>
2023-09-06 18:14:17 -0300

    CSS começou a ser configurado, o index (pagina de login) está com o css quase totalmene aplicado mas não funciona no forms.

---
+++ gl_cursos_default/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GlCursosDefaultConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gl_cursos_default'
---
+++ users/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+
+
+# Create your views here.
+
+def logout_views(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('gl_cursos_default:index'))
+
+def register(request):
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            authenticate_user = authenticate(username=new_user.username, password=request.POST['password1'])
+            login(request, authenticate_user)
+            return HttpResponseRedirect(reverse('gl_cursos_default:index'))
+    context = {'form':form}
+    return render(request, 'users/register.html', context)
